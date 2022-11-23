6326573db15a842adaa8ad97308d975794c5e9c0
Vazha Bichiashvili <vazha.bichiashvili@gmail.com>
2022-11-23 13:22:04 +0400

    Added visual characteristics to a searchBar

---
+++ src/components/SearchBar.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { View, TextInput, StyleSheet } from 'react-native';
+import { Feather } from '@expo/vector-icons';
+
+const SearchBar = () => {
+    return (
+        <View style={styles.backgroundStyle}>
+            <Feather name='search' style={styles.iconStyle} />
+            <TextInput style={styles.inputStyle} placeholder="search"/>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    backgroundStyle: {
+        marginTop: 10,
+        flexDirection: "row",
+        backgroundColor: '#F0EEEE',
+        height: 50,
+        borderRadius: 5,
+        marginHorizontal: 15
+    }, 
+    inputStyle: {
+        flex: 1,
+        fontSize: 18
+    },
+    iconStyle: {
+        fontSize: 35,
+        alignSelf: "center",
+        marginHorizontal: 15,
+        
+    }
+});
+
+export default SearchBar;
