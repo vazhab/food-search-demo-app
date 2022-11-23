2fe97ba14ed4fd7e6b8eb45d1c554ada71fb1d18
Vazha Bichiashvili <vazha.bichiashvili@gmail.com>
2022-11-23 17:50:05 +0400

    useEffect implementation
    Make an initial search until search button pressed

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from "../components/SearchBar";
 import yelp from "../api/yelp";
@@ -8,30 +8,31 @@ const SearchScreen = () => {
     const [results, setResults] = useState([]);
     const [errorMessage, setErrorMessage] = useState('');
 
-    const searchApi = async () => {
+    const searchApi = async (searchTerm) => {
         try {
             const response = await yelp.get('/search', {
                 params: {
                     limit: 50,
-                    term,
+                    term: searchTerm,
                     location: 'san jose',
-
-
                 }
             });
             setResults(response.data.businesses);
         } catch (err) {
             setErrorMessage('Something went wrong');
-
         }
     };
+
+    useEffect(() => {
+        searchApi('pasta');
+    }, [])
 
     return (
         <View>
             <SearchBar
                 term={term}
                 onTermChange={setTerm}
-                onTermSubmit={searchApi} />
+                onTermSubmit={() => searchApi(term)} />
             {errorMessage ? <Text>{errorMessage}</Text> : null}
             <Text>We Have Found {results.length} results</Text>
         </View>
