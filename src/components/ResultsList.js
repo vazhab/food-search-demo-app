5ff6d08589e3bd93b42b4d8eb1c7a83ccfda94a2
Vazha Bichiashvili <vazha.bichiashvili@gmail.com>
2022-11-24 08:50:03 +0400

    Added reusable details page

--- src/components/ResultsList.js
+++ src/components/ResultsList.js
@@ -1,15 +1,16 @@
 import React from "react";
 import { View, Text, StyleSheet, FlatList } from 'react-native';
+import ResultsDetail from "./ResultsDetail";
 
 const ResultsList = ({ title, results }) => {
     return <View>
         <Text style={styles.title}>{title}</Text>
-        <FlatList 
+        <FlatList
             horizontal
             data={results}
             keyExtractor={(result) => result.id}
-            renderItem={({ item }) =>  {
-                return <Text>{item.name}</Text>
+            renderItem={({ item }) => {
+                return <ResultsDetail result={item} />
             }}
         />
     </View>
